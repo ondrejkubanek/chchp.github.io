73192bb34d722cd0cabf3da02bc33a9f8f976f5d
ondrejkubanek <ondrakubanek2001@gmail.com>
2022-10-07 15:58:57 +0200

    scripty

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+var nav = document.querySelector('nav');
+var ico = document.getElementById('ico');
+if($(window).width() < 992){
+  ico.src="./img/logos/logo-black.png";
+  nav.classList.add('bg-light','shadow');}
+window.addEventListener("resize", myFunction);
+function myFunction() {
+  if($(window).width() < 992){
+  nav.classList.add('bg-light', 'shadow');
+  ico.src="./img/logos/logo-black.png";
+}else{
+  nav.classList.remove('bg-light', 'shadow');
+  ico.src="./img/logos/logo-white.png";
+
+}};
